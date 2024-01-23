e9207b484a15dd18879036819758bc39a2d7ed0e
ayush-1105 <104482219+ayush-1105@users.noreply.github.com>
2024-01-23 22:13:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Course Schedule/course-schedule.cpp
+++ Medium/Course Schedule/course-schedule.cpp
@@ -11,43 +11,50 @@ class Solution
     vector<int> findOrder(int n, int m, vector<vector<int>> prerequisites) 
     {
         //code here
-        vector<int>adj[n];
-        for(auto it:prerequisites)
-        {
-            adj[it[1]].push_back(it[0]);
-        }
+        vector<int> inDegree(n, 0);
+        unordered_map<int, list<int>> adj;
         
-        int indegree[n]={0};
-        for(int i = 0; i < n; i++)
-        {
-            for(auto it:adj[i])
-                indegree[it]++;
-        }
-        
-        queue<int>q;
-        for(int i = 0; i < n; i++)
-        {
-            if(indegree[i] == 0)
-                q.push(i);
-        }
-        
-        vector<int>topo;
-        while(!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
+        for(int i=0; i<m; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
             
-            for(auto it:adj[node])
-            {
-                indegree[it]--;
-                if(indegree[it] == 0)
-                    q.push(it);
+            adj[v].push_back(u);
+            inDegree[u]++;
+        }
+        
+        queue<int> q;
+        vector<int> ans;
+        
+        bool found = false;
+        for(int i=0; i<n; i++) {
+            if(inDegree[i] == 0) {
+                q.push(i);
+                found = true;
             }
         }
         
-        if(topo.size() == n) return topo;
-        return {};
+        if(!found) {
+            return ans;
+        }
+        
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            
+            ans.push_back(node);
+            
+            for(auto nbr: adj[node]) {
+                inDegree[nbr]--;
+                if(inDegree[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
+        }
+        
+        if(ans.size() != n) {
+            return {};
+        }
+        return ans;
     }
 };
 
