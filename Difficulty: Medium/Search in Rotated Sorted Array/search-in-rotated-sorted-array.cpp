cde58cb6977b8de6692c93e8fc49019346f0ebd5
ayush-1105 <104482219+ayush-1105@users.noreply.github.com>
2024-07-08 23:11:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -8,12 +8,37 @@ class Solution {
   public:
     int search(vector<int>& arr, int key) {
         // complete the function here
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i] == key){
-                return i;
+        int lo = 0;
+        int hi = arr.size() - 1;
+        
+        while(hi >= lo){
+            int mid = lo + (hi - lo) / 2;
+            
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[lo] <= arr[mid]){
+                if(arr[lo] <= key && arr[mid] > key){
+                    hi = mid - 1;
+                }else{
+                    lo = mid + 1;
+                }
+            }
+            else{
+                if(arr[hi] >= key && arr[mid] < key){
+                    lo = mid + 1;
+                }else{
+                    hi = mid - 1;
+                }
             }
         }
         return -1;
+        // for(int i=0; i<arr.size(); i++){
+        //     if(arr[i] == key){
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
 };
 
