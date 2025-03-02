436b0e88958c5b19db990ed510670b5e63ce7770
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-03-02 18:26:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,66 +1,59 @@
 //{ Driver Code Starts
-// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-// User function template for C++
 
 class Solution {
   public:
-    // Function to find maximum of each subarray of size k.
-    vector<int> max_of_subarrays(int k, vector<int> &arr) {
-        // your code here
-        int n = arr.size();
-        int i = 0, j = 1, maxi = 0;
-        vector<int> ans;
-        
-        while(i < k) {
-            if(arr[i] > maxi) maxi = arr[i];
-            i++;
-        }
-        ans.push_back(maxi);
-        
-        while(i < n) {
-            if(arr[j-1]==maxi)
-                maxi = *max_element(arr.begin()+j,arr.begin()+i+1);
-            else if(arr[i]>maxi) 
-                maxi = arr[i];
-            ans.push_back(maxi);
-            i++; j++;
-        }
-        
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        multiset<int>st;
+         for(int i=0;i<k;i++)st.insert(arr[i]);
+         vector<int>ans;
+         int j=0;
+         for(int i=k;i<arr.size();i++){
+            
+             ans.push_back(*st.rbegin());
+             st.erase(st.find(arr[j++]));
+             st.insert(arr[i]);
+         }
+        ans.push_back(*st.rbegin());
         return ans;
     }
 };
 
+
 //{ Driver Code Starts.
 
 int main() {
-
     int t;
     cin >> t;
-    cin.ignore();
+    cin.ignore(); // Ignore newline character after t
 
     while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
         vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
         }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
         Solution obj;
-        vector<int> res = obj.max_of_subarrays(k, arr);
+        vector<int> res = obj.maxOfSubarrays(arr, k);
         for (int i = 0; i < res.size(); i++)
             cout << res[i] << " ";
         cout << endl;
+        cout << "~"
+             << "\n";
     }
 
     return 0;
