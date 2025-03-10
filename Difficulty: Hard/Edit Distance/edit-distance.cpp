c872102d7697e5e59760ceb57cd8373af76a4244
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-03-10 18:26:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -1,58 +1,66 @@
 //{ Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int editDistance(string str1, string str2) {
-        // Code here
-        int m = str1.size();
-        int n = str2.size();
+    // Function to compute the edit distance between two strings
+    int solve(int i, int j , string & a, string & b,vector<vector<int>> & dp){
         
-        // Creating a 2D Vector DP 
-         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-        
-        // Iterating through words
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                // If i == 0, i.e STR1 is empty, meaning it will require j number of insertions
-                if(i == 0){
-                    dp[i][j] = j;
-                }
-                
-                // If j == 0; i.e STR2 is empty, meaning it will require i number of deletions
-                else if(j == 0){
-                    dp[i][j] = i;
-                }
-                
-                // We got the same word, so we'll do nothing and take back the result of previous
-                else if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                
-                // Word isn't matching so we'll take minimum of insertion, removal or replacement
-                else{
-                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                }
-            }
+        //we have to add the remaning string : 
+        if (i>=a.size()){
+            return b.size()-j;
         }
         
-        return dp[m][n];
+        //might be string b is smaller then string a to do that we need to remove all 
+        // the reming charecter of string a :
+        if (j>=b.size()){
+            return a.size()-i;
+        }
+        if (dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int equal = 0, dlt = 0, Insert = 0 , Replace_char_from_string_b = 0;
+        if (a[i]==b[j]){
+            equal = solve(i+1,j+1,a,b,dp);
+            return dp[i][j] = equal;//if you don't return here the value will not be 
+            //updated in other operation delet, insert, replace;  
+        }
+        else{
+            dlt =1 + solve(i+1,j, a,b,dp);
+            Insert = 1 + solve(i,j+1,a,b,dp);//assuming we inserted an correct charecter
+            Replace_char_from_string_b = 1 + solve(i+1,j+1,a,b,dp);
+        }
+        return dp[i][j] = min(dlt,min(Insert,Replace_char_from_string_b));
+    }
+    int editDistance(string& s1, string& s2) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(0,0,s1,s2,dp);
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
+
     int T;
     cin >> T;
+    cin.ignore();
     while (T--) {
-        string s, t;
-        cin >> s >> t;
+        string s1;
+        getline(cin, s1);
+        string s2;
+        getline(cin, s2);
         Solution ob;
-        int ans = ob.editDistance(s, t);
+        int ans = ob.editDistance(s1, s2);
         cout << ans << "\n";
+        cout << "~" << endl;
     }
     return 0;
 }
