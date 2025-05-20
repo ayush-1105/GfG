fd09045fa83011eedf918085cd908d1a39e363be
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-05-20 23:45:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -1,89 +1,6 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-
 /*
-struct Node {
+class Node {
+  public:
     int data;
     Node *left;
     Node *right;
@@ -96,62 +13,26 @@ struct Node {
 */
 class Solution {
   public:
-  int ans = 0;
-    pair<bool, int> help(Node* root, int target){
-        if (root == NULL){
-            return {false, 0};
-        }
-        auto lh = help(root -> left, target);
-        auto rh = help(root -> right, target);
-        if (lh.first){
-            ans = max(ans, lh.second + rh.second);
-            return {true, lh.second + 1};
-        }
-        else if (rh.first){
-            ans = max(ans, rh.second + lh.second);
-            return {true, rh.second + 1};
-        }
-        else if (target == root -> data){
-            ans = max(ans, max(lh.second, rh.second));
-            return {true, 1};
-        }
-        else{
-            return {false, max(lh.second, rh.second) + 1};
-        }
+    void dfs(Node* node, Node* par, map<Node*, Node*>& parent, int target, Node*& startNode){
+    	if(node==NULL) return;
+    	parent[node]=par;
+        if(node->data==target) startNode=node;
+        dfs(node->left, node, parent, target, startNode);
+        dfs(node->right, node, parent, target, startNode);
     }
-    int minTime(Node* root, int target) 
-    {
-        // Your code goes here
-         help(root, target);
-        return ans;
+    void dfs2(Node* node, Node* par, int time, int& totalTime, map<Node*, Node*>& parent){
+    	if(node==NULL) return;
+    	totalTime=max(totalTime, time);
+    	if(node->left!=par) dfs2(node->left, node, time+1, totalTime, parent);
+    	if(node->right!=par) dfs2(node->right, node, time+1, totalTime, parent);
+    	if(parent.find(node)!=parent.end() && parent[node]!=par) dfs2(parent[node], node, time+1, totalTime, parent);
+    }
+    int minTime(Node* root, int target) {
+        map<Node*, Node*> parent;
+        Node* startNode=nullptr;
+        dfs(root, NULL, parent, target, startNode);  // O(V+E)
+        int totalTime=0;
+        dfs2(startNode, NULL, 0, totalTime, parent); // O(V+E)
+        return totalTime;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() 
-{
-    int tc;
-    scanf("%d ", &tc);
-    while (tc--) 
-    {    
-        string treeString;
-        getline(cin, treeString);
-        // cout<<treeString<<"\n";
-        int target;
-        cin>>target;
-        // cout<<target<<"\n";
-
-        Node *root = buildTree(treeString);
-        Solution obj;
-        cout<<obj.minTime(root, target)<<"\n"; 
-
-        cin.ignore();
-
-    }
-
-
-    return 0;
-}
-
-// } Driver Code Ends
