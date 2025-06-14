8d45234f82ab95c440e8d2e149992358413f3478
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-06-14 18:34:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.cpp
@@ -0,0 +1,39 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    void inorder(vector<int>&ans, Node*root){
+      if(root==NULL){
+          return;
+      }
+      inorder(ans,root->left);
+      ans.push_back(root->data);
+      inorder(ans,root->right);
+  }
+    bool isSymmetric(Node* root) {
+        bool ansi=true;
+        vector<int>ans;
+        inorder(ans,root);
+        int i=0;
+        int j=ans.size()-1;
+        while(i<=j){
+            if(ans[i]!=ans[j]){
+                ansi=false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        return ansi;
+    }
+};
