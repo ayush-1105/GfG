f3ec356aac45473f1368f93630efefb43c976d9f
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-08-15 16:19:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.cpp
+++ Difficulty: Medium/Insert Interval/insert-interval.cpp
@@ -1,73 +1,31 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
     vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
                                        vector<int> &newInterval) {
         // code here
-        vector<vector<int>> result;
-    int i = 0, n = intervals.size();
-    
-    // Case 1: Add all intervals that end before the new interval starts
-    while (i < n && intervals[i][1] < newInterval[0]) {
-        result.push_back(intervals[i]);
-        i++;
-    }
-    
-    // Case 2: Merge all overlapping intervals
-    while (i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = min(newInterval[0], intervals[i][0]);
-        newInterval[1] = max(newInterval[1], intervals[i][1]);
-        i++;
-    }
-    
-    // Add the merged newInterval to the result
-    result.push_back(newInterval);
-    
-    // Case 3: Add all remaining intervals that start after the new interval ends
-    while (i < n) {
-        result.push_back(intervals[i]);
-        i++;
-    }
-    
-    return result;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> mergedintervals;
+        mergedintervals.push_back({intervals[0][0],intervals[0][1]});
+        int n = intervals.size();
+        for(int i = 1;i<n;i++)
+        {
+            int s = mergedintervals.back()[0];
+            int e = mergedintervals.back()[1];
+            mergedintervals.pop_back();
+            int cs = intervals[i][0];
+            int ce = intervals[i][1];
+            
+            if(e>=cs)
+            {
+                mergedintervals.push_back({min(s,cs),max(e,ce)});
+            }
+            else
+            {
+                mergedintervals.push_back({s,e});
+                mergedintervals.push_back({cs,ce});
+            }
+        }
+        return mergedintervals;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<vector<int>> intervals(N, vector<int>(2));
-        for (int i = 0; i < N; i++) {
-            cin >> intervals[i][0] >> intervals[i][1];
-        }
-        vector<int> newInterval(2);
-        cin >> newInterval[0] >> newInterval[1];
-
-        Solution obj;
-        vector<vector<int>> ans = obj.insertInterval(intervals, newInterval);
-        cout << "[";
-        for (int i = 0; i < ans.size(); i++) {
-            cout << "[" << ans[i][0] << ',' << ans[i][1] << ']';
-            if (i != (ans.size() - 1))
-                cout << ",";
-        }
-        cout << "]" << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
