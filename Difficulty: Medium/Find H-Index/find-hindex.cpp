e09e690abdcde300ba3d3c039ee7ecb890ca9fb5
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-08-18 22:38:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,60 +1,31 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
-    // Function to find hIndex
+    int check(int mid,vector<int>&citations)
+  {
+      int count=0;
+      for(int i=0;i<citations.size();i++)
+      {
+          if(citations[i]>=mid)count++;
+      }
+      return count;
+  }
     int hIndex(vector<int>& citations) {
-        // code here
-        sort(citations.begin(), citations.end());
-        int n=citations.size()-1;
-        int cnt=0;
-        int maxii = INT_MIN;
-        for(int i=n;i>=0;i--){
-            cnt ++;
-            
-            if(cnt >= citations[i]){
-                
-                return max(maxii,min(cnt,citations[i]));
+        int n=citations.size();
+        int l=1,h=n;
+        int ans=0;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(check(mid,citations)>=mid)
+            {
+                ans=mid;
+                l=mid+1;
             }
-            maxii = max(maxii, cnt);
+            else if(check(mid,citations)<mid)
+            {
+                h=mid-1;
+            }
         }
-        return maxii;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int test_cases;
-    cin >> test_cases;
-    cin.ignore();
-    while (test_cases--) {
-        string input;
-        vector<int> arr;
-
-        // Read the array from input line
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        // Solution instance to invoke the function
-        Solution ob;
-        int result = ob.hIndex(arr);
-
-        cout << result << endl;
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
