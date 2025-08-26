f20cf1f001cece8e37ea8a77d3de8977387faa7b
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-08-27 00:02:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -1,55 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to count the number of possible triangles.
     int countTriangles(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        int cnt = 0;
+        int n=arr.size();
+        int ans=0;
         sort(arr.begin(),arr.end());
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n ;j++){
-                int sum = arr[i]+arr[j];
-                for(int k=j+1;k<n ;k++){
-                    if(sum>arr[k]){
-                        cnt++;
-                    }
+            for(int j=i+1;j<n-1;j++){
+                int sum=arr[i]+arr[j];
+                int index = std::lower_bound(arr.begin(), arr.end(), sum) - arr.begin();
+                if(index==-1)
+                    index=n-1;
+                if(index-1>j){
+                    //cout<<index-1<<endl;
+                    //cout<<"sum="<<sum<<" "<<((index-1)-j)<<endl;
+                    ans+=((index-1)-j);
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.countTriangles(a) << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
