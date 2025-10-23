93bc9c9ffba3be1b13bee2f9899534ce7449ad50
PANDE AYUSH ARJUN <104482219+ayush-1105@users.noreply.github.com>
2025-10-23 19:45:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -1,54 +1,31 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        // Your code here
-        priority_queue<pair<int,vector<int>>>maxHeap;
-        for(auto &i:points){
-            int x=i[0],y=i[1];
-            int dist=x*x+y*y;
-            maxHeap.push({dist,i});
-            if(maxHeap.size()>k) maxHeap.pop();
+        // code here
+        priority_queue<pair<double,pair<int,int>>>pq;
+        
+        for(auto p:points){
+            int x=p[0];
+            int y=p[1];
+            
+             double d=(double)sqrt(x*x+y*y);
+             
+             pq.push({d,{x,y}});
+             
+             if(pq.size()>k){
+                 pq.pop();
+             }
         }
-        vector<vector<int>>ans;
-        while(!maxHeap.empty()){
-            ans.push_back(maxHeap.top().second);
-            maxHeap.pop();
+        
+        
+        
+        vector<vector<int>>res;
+        
+        while(!pq.empty()){
+            res.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
         }
-        return ans;
+        
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int k;
-        cin >> k;
-        int n;
-        cin >> n;
-        vector<vector<int>> points(n, vector<int>(2));
-        for (int i = 0; i < n; i++) {
-            cin >> points[i][0] >> points[i][1];
-        }
-        Solution ob;
-        vector<vector<int>> ans = ob.kClosest(points, k);
-        sort(ans.begin(), ans.end());
-        for (const vector<int>& point : ans) {
-            cout << point[0] << " " << point[1] << endl;
-        }
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
